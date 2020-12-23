89fe6a59a3f6d0840a20e012a49bc26172bf411b
Nathália Ornelas <38090460+MitchieX@users.noreply.github.com>
2020-12-22 23:42:53 -0300

    Add files via upload

---
+++ solog old/README.md
@@ -0,0 +1,2 @@
+# SOLOG
+Projeto realizado para a SOLOG Soluções em 2018
---
+++ solog old/mail.php
@@ -0,0 +1,44 @@
+<?php 
+	$errors = '';
+	$myemail = 'denis@sologsl.com';//<-----Put Your email address here.
+	if(empty($_POST['name'])  ||
+	   empty($_POST['email']) ||
+	   empty($_POST['message']))
+	{
+	    $errors .= "\n Erro: Todos os campos são requeridos";
+	}
+	$name = $_POST['name'];
+	$email_address = $_POST['email'];
+	$message = $_POST['message'];
+	if (!preg_match(
+	"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+	$email_address))
+	{
+	    $errors .= "\n Erro: Email Inválido";
+	}
+
+	if( empty($errors))
+	{
+	$to = $myemail;
+	$email_subject = "Formulario de Contato: $name";
+	$email_body = "Você recebeu uma nova mensagem. ".
+	"Aqui estão os detalhes:\n Nome: $name \n ".
+	"Email: $email_address\n Menssagem: \n $message";
+	$headers = "Para: $myemail\n";
+	$headers .= "De: $email_address";
+	$send_mail = mail($to,$email_subject,$email_body,$headers);
+	if($send_mail){
+	    header('Location: index.html');//redireciona para onde quiser, neste caso, para index.html
+	    echo "<script>alert('E-mail enviado com sucesso!');</script>";  //alert enviado!
+	} else {
+		//redireciona para onde quiser, neste caso, para index.html
+	    header('Location: index.html');
+	    //alert não enviado!
+	    echo "<script>alert('Falha ao enviar e-mail.');</script>";
+	    
+	}
+
+}
+?>
+
+
